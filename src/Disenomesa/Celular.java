0cfd891fc69093ec8ca33a8ffefa3cc8e66b951e
Valeria <valeria@hotmail.com>
2019-02-04 22:00:13 -0600

    Primer commit del codigo en Java

---
+++ src/Disenomesa/Celular.java
@@ -0,0 +1,14 @@
+package Disenomesa;
+
+public class Celular {
+   public Celular() {
+    }
+    public String Medidas;
+    public String Memoria;
+    public String Pantalla;
+    public String Bateria;
+    public String Sistema;
+    public int Precio;
+    public String Camara;
+    public String Procesador;  
+}
---
+++ src/Disenomesa/showBehavior.java
@@ -0,0 +1,5 @@
+
+package Disenomesa;
+public interface showBehavior {
+     public void showIcons();
+}
