dfe6b87ade95a6daff1521980dda4a50ca54fc4b
Valeria <valeria@hotmail.com>
2019-02-12 10:00:39 -0600

    clases celulares

---
+++ src/Mesa/MotoG5.java
@@ -0,0 +1,16 @@
+package Mesa;
+public class MotoG5 extends Celular {
+    public MotoG5() {
+    }
+    public void setInfo(){ 
+      Medidas="75x74";
+      Memoria="10 gb";
+      Pantalla="7421";
+      Bateria="100";
+      Sistema="Android";
+      VSistema="Oreo 1.9";
+      Precio="$43456";
+      Camara="213";
+      Procesador="4.5";  
+    }
+}
---
+++ src/Mesa/Nexus6P.java
@@ -0,0 +1,16 @@
+package Mesa;
+public class Nexus6P extends Celular {
+    public Nexus6P() {
+    }
+    public void setInfo(){ 
+      Medidas="75x74";
+      Memoria="10 gb";
+      Pantalla="7421";
+      Bateria="100";
+      Sistema="Android";
+      VSistema="Oreo 1.9";
+      Precio="$43456";
+      Camara="213";
+      Procesador="4.5";  
+    }
+}
